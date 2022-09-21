53611b16cb15a30c87ccc3c92ed44645e2c69433
anand-CodeBin <anand@code-b.in>
2022-09-21 20:28:31 +0530

    fix: some thing here broke
    something here broke and i just fixed thart
    
    BREAKING CHANGE: shall break the image component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./App.css";
+import { store } from "./Redux/store";
+import { Provider as ReduxProvider } from "react-redux";
+import RootNavigator from "./navigation";
+
 function App() {
-  return <h1>Hello world !!</h1>;
+  return (
+    <ReduxProvider store={store}>
+      <RootNavigator />
+    </ReduxProvider>
+  );
 }
 
 export default App;
---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+import { Theme } from "../models";
+
+const getTheme = (theme: Theme) => {
+  // theme logic
+  return theme;
+};
+
+export default { getTheme };
